3af7cc6fa4ee2c017fa7849f13f6ef81f551c1ea
Gurpreet Singh <Guppst3r@gmail.com>
2015-09-19 14:07:46 -0400

    exploring the drawer

--- Everything4Scale/MyApplication/app/src/main/java/com/example/singh/everything4scale/MainActivity.java
+++ Everything4Scale/MyApplication/app/src/main/java/com/example/singh/everything4scale/MainActivity.java
@@ -59,13 +59,13 @@ public class MainActivity extends ActionBarActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = "Banana";
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                mTitle = "Orange";
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
+                mTitle = "Add More";
                 break;
         }
     }
